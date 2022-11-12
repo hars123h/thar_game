518e15761c8b8fa157c259505cbebec7962e5308
unknown <mprakhar35@gmail.com>
2022-11-12 16:04:29 +0530

    ui commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = () => {
             <div className="title text-white font-bold">Office computer No.1</div>
             <div className="text-xs text-red-700 font-bold mb-4">daily withdraws</div>
             <div className="basic_info text-white">Project Amount: &#8377;500</div>
-            <div className="basic_info  text-white">Daily Earning: &#8377;50</div>
+            <div className="basic_info  text-white">Daily Earnings: &#8377;50</div>
             <div className="basic_info  text-white">Project Cycle: &#8377;365 day</div>
             <div className="basic_info  text-white">Total Earning: &#8377;18250</div>
             <div  className="cursor-pointer btn text-white text-center p-2 mt-1 text-lg rounded-md  w-4/5 mx-auto bg-[#00bcd4]">Click to buy</div>
