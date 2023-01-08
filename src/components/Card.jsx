cf4493275061c69ae1176ff24466fc201451bf3a
unknown <mprakhar35@gmail.com>
2023-01-08 23:02:18 +0530

    Added some details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,12 +10,12 @@ const Card = ({product_type, product_image, plan_name, plan_type, plan_amount, p
         <div className="info  bg-yellow-500 p-2 text-sm">
           
             <div className="title text-white font-bold">{plan_name}</div>
-            <div className="text-xs text-red-700 font-bold mb-4">Daily Withdrawals</div>
+            {product_type==='long' && (<div className="text-xs text-red-700 font-bold mb-4">Daily Withdrawals</div>)}
             <div className="basic_info text-white">Project Amount: &#8377;{new Intl.NumberFormat().format(plan_amount)}</div>
             <div className="basic_info  text-white">Daily Earnings: &#8377;{new Intl.NumberFormat().format(plan_daily_earning)}</div>
             <div className="basic_info  text-white">Project Cycle: {plan_cycle} days</div>
             <div className="basic_info  text-white">Total Earning: &#8377;{new Intl.NumberFormat().format(plan_cycle*plan_daily_earning)}</div>
-            <div  className="cursor-pointer btn text-white text-center p-2 mt-1 text-lg rounded-md  w-4/5 mx-auto bg-green-600" onClick={()=>handleClick(product_type ,plan_name, plan_type, plan_amount, plan_daily_earning, plan_cycle)}>Click to buy</div>
+            <div className="cursor-pointer btn text-white text-center p-2 mt-1 text-lg rounded-md  w-4/5 mx-auto bg-green-600" onClick={()=>handleClick(product_type ,plan_name, plan_type, plan_amount, plan_daily_earning, plan_cycle)}>Click to buy</div>
         </div>
     </div>
   )
